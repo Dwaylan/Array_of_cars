d3b7f2c66f1d23b3fb7f24046dd4cc4059185e78
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-08-20 11:39:23 -0500

    finished assignment

--- Array_of_cars.js
+++ Array_of_cars.js
@@ -37,3 +37,52 @@ console.log(totalCars)
 //array totalCars in reverse.
 let carsInReverse = totalCars.reverse()
 console.log(carsInReverse)
+
+// .sort() - Use the sort method to put the array carsInReverse into alphabetical order.
+// Based on the types of cars you used, predict which item in the array should 
+//be at index 0.
+carsInReverse.sort()
+console.log(carsInReverse)
+
+// .slice() - Use the slice method to remove Ford and Honda from the carsInReverse 
+//array and move them into a new array called removedCars.
+let removedCars = carsInReverse.slice(2, 6)
+console.log(removedCars)
+
+let removedCars2 = carsInReverse.splice(1, 2,)
+carsInReverse.splice(1, 2, 'ford', 'honda')
+console.log(removedCars2)
+console.log(carsInReverse)
+
+// .push() - Use the push method to add the types of cars that you removed using the 
+//splice method to the carsInReverse array.
+carsInReverse.push('buick', 'chrysler')
+console.log(carsInReverse)
+
+// .pop() - Use the pop method to remove and console.log the last item in the array
+// carsInReverse.
+console.log(carsInReverse.pop())
+
+// .shift() - Use the shift method to remove and console.log the first item in the
+// array carsInReverse.
+console.log(carsInReverse.shift())
+
+// .unshift() - Use the unshift method to add a new type of car to the array 
+//carsInReverse.
+carsInReverse.unshift('Saab')
+console.log(carsInReverse)
+
+
+// .forEach() - Create an array called numbers with the following items: 23, 45, 0, 2
+// Write code that will add 2 to each item in the array numbers.
+// .forEach() requires a function to be passed into it as its first argument. 
+//Build a function that will add 2 and then use .forEach() to pass each 
+//number into your freshly built function. 
+//const numbers = [23, 45, 0 , 2, 8, 44, 100, 1, 3, 91, 34]
+
+const numbers = [23, 45, 0 , 2, 8, 44, 100, 1, 3, 91, 34]
+
+
+numbers.forEach(function(i) {
+    console.log(i + 2)
+})
